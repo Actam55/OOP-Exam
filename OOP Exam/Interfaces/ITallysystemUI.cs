014e6e9ea37de624d2a692c0e3b64b5379ae6081
Actam <l.1999@hotmail.dk>
2021-12-14 21:33:14 +0100

    Removed comment in ITallySystemUI

--- OOP Exam/Interfaces/ITallysystemUI.cs
+++ OOP Exam/Interfaces/ITallysystemUI.cs
@@ -14,7 +14,6 @@ namespace OOP_Exam.Interfaces
         void DisplayUserInfo(User user);
         void DisplayTooManyArgumentsError(string command);
         void DisplayAdminCommandNotFoundMessage(string adminCommand);
-        //void DisplayUserBuysProduct(BuyTransaction transaction);
         void DisplayUserBuysProduct(int count, BuyTransaction transaction);
         void Close();
         void DisplayInsufficientCash(User user, Product product, int amount);
